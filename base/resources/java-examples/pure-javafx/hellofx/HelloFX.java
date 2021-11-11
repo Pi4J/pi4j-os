069711a0cb50cf34383d15aed59c11b1214f53db
Pascal Mathis <mail@pascalmathis.com>
2021-11-11 10:31:17 +0100

    feat: re-enable audio in HelloFX with MediaView

--- base/resources/java-examples/pure-javafx/hellofx/HelloFX.java
+++ base/resources/java-examples/pure-javafx/hellofx/HelloFX.java
@@ -8,9 +8,10 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-//import javafx.scene.media.AudioClip;
-//import javafx.scene.media.Media;
-//import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 /**
@@ -18,11 +19,10 @@ import javafx.stage.Stage;
  *
  * It's a long way from here to a real application.
  *
- * But as long as 'HelloFX' isn't working properly, it's useless to go any further.
- *
  * It's not meant to be any kind of template to start your development.
  *
- * Initially copied from https://github.com/openjfx/samples/blob/master/CommandLine/Modular/CLI/hellofx/src/hellofx/HelloFX.java
+ * Initially copied from
+ * https://github.com/openjfx/samples/blob/master/CommandLine/Modular/CLI/hellofx/src/hellofx/HelloFX.java
  *
  * @author Dieter Holz
  */
@@ -30,27 +30,28 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion   = System.getProperty("java.version");
+        String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
 
-//        MediaPlayer mediaPlayer = new MediaPlayer(new Media(getClass().getResource("bgm.mp3").toExternalForm()));
-//        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-//        mediaPlayer.play();
+        MediaPlayer mediaPlayer = new MediaPlayer(new Media(getClass().getResource("bgm.mp3").toExternalForm()));
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setAutoPlay(true);
+        MediaView mediaView = new MediaView(mediaPlayer);
 
         Label lbl = new Label("JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 
-//        AudioClip dropSound = new AudioClip(getClass().getResource("drop.wav").toExternalForm());
+        AudioClip dropSound = new AudioClip(getClass().getResource("drop.wav").toExternalForm());
         Button btn = new Button("Say Hello");
         btn.setOnAction(event -> {
             lbl.setText("Hello");
-//            dropSound.play();
+            dropSound.play();
         });
 
         ImageView imgView = new ImageView(new Image(HelloFX.class.getResourceAsStream("openduke.png")));
         imgView.setFitHeight(200);
         imgView.setPreserveRatio(true);
 
-        VBox rootPane = new VBox(50, imgView, lbl, btn);
+        VBox rootPane = new VBox(50, mediaView, imgView, lbl, btn);
         rootPane.setAlignment(Pos.CENTER);
 
         rootPane.getStylesheets().add(HelloFX.class.getResource("styles.css").toExternalForm());
