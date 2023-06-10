5c3610c134ad2d9774c92d02d67f601e5a38be25
DieterHolz <dieter.holz@fhnw.ch>
2023-06-10 13:38:06 +0200

    add a default user

--- base/base.sh
+++ base/base.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+declare username=pi
+declare password=crowpi
+adduser  --gecos "" --disabled-password $username
+chpasswd <<<"$username:$password"
+sudo -i -u "$username"
+
+
 set -euxo pipefail
 
 # Script configuration
@@ -74,25 +81,25 @@ install -Dm 0644 /tmp/res-base/system/pi4j-update-mime-db.service /etc/systemd/s
 systemctl enable pi4j-update-mime-db.service
 
 # Disable getting started wizard
-#rm /etc/xdg/autostart/piwiz.desktop
+rm /etc/xdg/autostart/piwiz.desktop
 
 # Disable screen blanking by default
 mkdir -p /etc/X11/xorg.conf.d/
 cp /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/
 
 # Remove default backgrounds
-#rm /usr/share/rpd-wallpaper/*.jpg
+rm /usr/share/rpd-wallpaper/*.jpg
 
 # Override system-wide default wallpaper
-# sed -i 's/wallpaper=.*/wallpaper=\/opt\/pi4j-os\/wallpaper-static.jpg/g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-*.conf
-# sed -i 's/wallpaper_mode=.*/wallpaper_mode=stretch/g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-*.conf
+sed -i 's/wallpaper=.*/wallpaper=\/opt\/pi4j-os\/wallpaper-static.jpg/g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-*.conf
+sed -i 's/wallpaper_mode=.*/wallpaper_mode=stretch/g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-*.conf
 
 # Deploy dynamic wallpaper script and resources
-# sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-autostart.desktop /home/pi/.config/autostart/pi4j-os-wallpaper.desktop
-# sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-systemd.service /home/pi/.config/systemd/user/pi4j-os-wallpaper.service
-# sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-systemd.path /home/pi/.config/systemd/user/pi4j-os-wallpaper.path
-#install -Dm 0755 /tmp/res-base/wallpaper/wallpaper-hook.sh /lib/dhcpcd/dhcpcd-hooks/99-pi4j-os
-#install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-static.jpg /opt/pi4j-os/wallpaper-static.jpg
+sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-autostart.desktop /home/pi/.config/autostart/pi4j-os-wallpaper.desktop
+sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-systemd.service /home/pi/.config/systemd/user/pi4j-os-wallpaper.service
+sudo -u pi install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-systemd.path /home/pi/.config/systemd/user/pi4j-os-wallpaper.path
+install -Dm 0755 /tmp/res-base/wallpaper/wallpaper-hook.sh /lib/dhcpcd/dhcpcd-hooks/99-pi4j-os
+install -Dm 0644 /tmp/res-base/wallpaper/wallpaper-static.jpg /opt/pi4j-os/wallpaper-static.jpg
 
 # Deploy java-kiosk helper script for JavaFX apps
 sudo install -Dm 0755 /tmp/res-base/java/java-kiosk.py /usr/local/bin/java-kiosk
