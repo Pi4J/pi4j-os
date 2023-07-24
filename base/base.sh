f500405797afff148bd1f377de6143b6f4f7b96b
DieterHolz <dieter.holz@fhnw.ch>
2023-07-24 12:03:06 +0200

    upgrade JavaFX to 20.0.2

--- base/base.sh
+++ base/base.sh
@@ -8,8 +8,8 @@
 set -euxo pipefail
 
 # Script configuration
-declare -gr JDK="17.0.7-tem"
-declare -gr GLUON_JAVAFX_VERSION="20.0.1"
+declare -gr JDK="17.0.8-tem"
+declare -gr GLUON_JAVAFX_VERSION="20.0.2"
 declare -gr GLUON_JAVAFX_URL="https://download2.gluonhq.com/openjfx/${GLUON_JAVAFX_VERSION}/openjfx-${GLUON_JAVAFX_VERSION}_monocle-linux-aarch64_bin-sdk.zip"
 declare -gr GLUON_JAVAFX_PATH="/opt/javafx-sdk"
 declare -gr GLUON_JAVAFX_VERSION_PATH="/opt/javafx-sdk-${GLUON_JAVAFX_VERSION}"
