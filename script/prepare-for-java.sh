cca5723c023a05012d51fe24d8eb0160189116b8
Frank Delporte <frank@webtechie.be>
2024-11-13 11:18:16 +0100

    Extra logging

--- script/prepare-for-java.sh
+++ script/prepare-for-java.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # System updates
+
 echo "Update the list of available packages and their versions"
 apt update
+
+echo "   "
+echo "-------------------------"
+echo "   "
+
 echo "Install the newest versions of all currently installed packages that have available updates"
 apt upgrade -y
 
+echo "   "
+echo "-------------------------"
+echo "   "
+
 # Configuration changes
+
 echo "Apply configuration changes to easier interact with electronic components"
 raspi-config nonint do_i2c 0
 raspi-config nonint do_ssh 0
@@ -16,11 +27,21 @@ raspi-config nonint do_onewire 0
 systemctl disable hciuart
 echo "dtoverlay=disable-bt" | tee -a /boot/firmware/config.txt
 
-# Missing Dependencies
+echo "   "
+echo "-------------------------"
+echo "   "
+
+# Missing dependencies
+
 echo "Install missing dependencies"
 apt install -y i2c-tools vim git java-common libxi6 libxrender1 libxtst6
 
+echo "   "
+echo "-------------------------"
+echo "   "
+
 # Install Java (SDK with JavaFX) and related tools
+
 echo "Install Java"
 wget https://cdn.azul.com/zulu/bin/zulu21.38.21-ca-fx-jdk21.0.5-linux_arm64.deb
 dpkg -i zulu21.38.21-ca-fx-jdk21.0.5-linux_arm64.deb
@@ -28,18 +49,36 @@ rm zulu21.38.21-ca-fx-jdk21.0.5-linux_arm64.deb
 echo "Installed Java version:"
 java -version
 
+echo "   "
+echo "-------------------------"
+echo "   "
+
 echo "Install SDKMAN"
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 echo "Installed SDKMAN version:"
 sdk version
 
+echo "   "
+echo "-------------------------"
+echo "   "
+
 echo "Install Maven"
 sdk install maven
 echo "Installed Maven version:"
 mvn -v
 
+echo "   "
+echo "-------------------------"
+echo "   "
+
 echo "Install JBang"
 sdk install jbang
 echo "Installed JBang version:"
 jbang --version
+
+echo "   "
+echo "-------------------------"
+echo "   "
+
+echo "Done! Have fun..."
